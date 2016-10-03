b719f449aab717deb45dd60d348b1a2504b1f8d0
Lars Brix <laxisbrix@gmail.com>
2016-10-03 18:56:19 +0200

    add alias component

---
+++ src/app/components/Detail/Alias.js
@@ -0,0 +1,16 @@
+const m = require("mithril");
+
+module.exports = {
+	view: ({ attrs }) => (
+		<div className="alias">
+			<div className="alias-time">
+			{
+				attrs.alias.created_at
+					? attrs.alias.created_at.toDateString()
+					: "no Date"
+			}
+			</div>
+			<div className="alias-name">{attrs.alias.name}</div>
+		</div>
+	)
+};
