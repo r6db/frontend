2babc36e00fdea84f63bdb8ad03cb6d0ba6ca590
Lars Brix <laxisbrix@gmail.com>
2016-10-03 18:58:07 +0200

    add debugging statements to index

--- src/app/index.js
+++ src/app/index.js
@@ -2,3 +2,11 @@
  * this is the app's entry point
  */
 require("./app");
+
+if(process.env.NODE_ENV === "development") {
+	const page = require("page");
+	window.page = page;
+
+	const m = require("mithril");
+	window.m = m;
+}
