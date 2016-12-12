949822abefd7bcc49c9f6d529e3a1f4d822b2a97
Lars Brix <lars.brix@vispiron.de>
2016-12-12 10:19:28 +0100

    remove absolute url from api code

--- src/app/lib/constants.js
+++ src/app/lib/constants.js
@@ -2,7 +2,7 @@ export const appname = "ggs";
 export const appid = "r6db-frontend";
 export const title ="R6DB - Rainbow 6 PC Player database";
 export const baseurl ="https://r6db.com/";
-export const v2Api ="https://r6db.com/api/v2";
+export const v2Api ="/api/v2";
 export const State =Object.freeze({
     INITIAL: "is-initial",
     SEARCH: "is-searching",
