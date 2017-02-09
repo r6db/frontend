a8148bc84637cd5f027fe90dc9f3667be9e2809e
Lars Brix <lars.brix@vispiron.de>
2017-02-09 14:57:18 +0100

    remove unneeded return statemnt

--- src/app/lib/api/methods/getTweets.js
+++ src/app/lib/api/methods/getTweets.js
@@ -6,15 +6,12 @@ export default function () {
     return fetch(`${v2Api}/twitter`, { headers: getHeaders() })
         .then(failEarly)
         .then(res => res.json())
-        .then(tweets => tweets.map(tweet => {
-            
-            return {
-                time: moment(tweet.created_at, "ddd MMM DD HH:mm:ss Z YYYY").format(DATE_LONG),
-                html: (tweet.entities.media || [])
-                    .concat(tweet.entities.urls || [])    
-                    .reduce((acc, curr) => acc
-                        .replace(curr.url, `<a href="${curr.expanded_url}">${curr.url}</a>`
+        .then(tweets => tweets.map(tweet => ({
+            time: moment(tweet.created_at, "ddd MMM DD HH:mm:ss Z YYYY").format(DATE_LONG),
+            html: (tweet.entities.media || [])
+                .concat(tweet.entities.urls ||  [])
+                .reduce((acc, curr) => acc
+                    .replace(curr.url, `<a href="${curr.expanded_url}">${curr.url}</a>`
                     ), tweet.text)
-            };
-        }));
+        })));
 };
