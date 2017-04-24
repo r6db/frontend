4bfa63702767664699db88e2464c9fe569d969ad
Lars Brix <lars.brix@vispiron.de>
2017-04-24 08:25:52 +0200

    Fix typo

--- src/app/components/Detail/Errors/NoPlaytime.js
+++ src/app/components/Detail/Errors/NoPlaytime.js
@@ -7,7 +7,7 @@ export default {
         return (
             <DetailError title={attrs.name}>
                 <p>This Profile has not yet played the Game!</p>
-                <p>Either its the first time we see this account (and it'l work in a few minutes) or this is really a profile that hasn't played R6. in that case, it'll be removed from our database at some point.</p>
+                <p>Either its the first time we see this account (and it'll work in a few minutes) or this really is a profile that hasn't played R6. In that case, it'll be removed from our database at some point.</p>
             </DetailError>
         );
     }
