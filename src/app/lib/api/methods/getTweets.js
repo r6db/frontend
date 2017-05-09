0036f641f8c16e81b0be6c26c98eafb2fa947b3a
Lars Brix <lars.brix@vispiron.de>
2017-05-09 07:48:38 +0200

    Use smaller tweet font-size on mobile

--- src/app/lib/api/methods/getTweets.js
+++ src/app/lib/api/methods/getTweets.js
@@ -6,13 +6,16 @@ export default function () {
     return fetch(`${v2Api}/twitter`, { headers: getHeaders() })
         .then(failEarly)
         .then(res => res.json())
-        .then(tweets => tweets.map(tweet => ({
-            time: moment(tweet.created_at, "ddd MMM DD HH:mm:ss Z YYYY").format(DATE_LONG),
-            url: console.log(tweet) || `https://twitter.com/Rainbow6_DB/status/${tweet.id_str}`,
-            html: (tweet.entities.media || [])
-                .concat(tweet.entities.urls ||  [])
-                .reduce((acc, curr) => acc
-                    .replace(curr.url, `<a href="${curr.expanded_url}">${curr.url}</a>`
-                    ), tweet.text)
-        })));
+        .then(tweets =>
+            tweets.map(tweet => {
+                const time = moment(tweet.created_at, "ddd MMM DD HH:mm:ss Z YYYY").format(DATE_LONG);
+                const url = `https://twitter.com/Rainbow6_DB/status/${tweet.id_str}`;
+                const html = (tweet.entities.media || [])
+                    .concat(tweet.entities.urls || [])
+                    .reduce((acc, curr) => acc
+                        .replace(curr.url, `<a href="${curr.expanded_url}">${curr.url}</a>`
+                        ), tweet.text);
+                return { time, url, html };
+            })
+        );
 }
