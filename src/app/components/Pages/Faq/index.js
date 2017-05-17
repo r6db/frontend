d4d7d2e0d10d1960649aa84c3ac64fd52df36f26
Naughtimus Muppetus <info@gitgudscrub.com>
2017-05-17 20:49:02 +0300

    fix: proofread faq

--- src/app/components/Pages/Faq/index.js
+++ src/app/components/Pages/Faq/index.js
@@ -38,16 +38,16 @@ export default {
                         When you start out, you have a skill-rating of 25. This value gets updated by playing. Basically, the better you perform, the higher your skill-rating.
                     </p>
                     <p>
-                        Because such system can't be perfect, another variable exists: <i>uncertainty</i>. As the name suggests, uncertainty models how accurate your skill-rating is. As you play, the uncertainty value constantly falls and approaches zero: "Perfect accuracy". The Points you see in-game is called the mmr (or MatchMaking Rating). This value is calculated from your skill-rating and up until Red Crow also took your playtime into consideration. <br/>
+                        Because such system can't be perfect, another variable exists: <i>uncertainty</i>. As the name suggests, uncertainty models how accurate your skill-rating is. As you play, the uncertainty value constantly falls and approaches zero: "Perfect certainty". The points you see in-game is called the mmr (MatchMaking Rating). This value is calculated from your skill-rating, and up until Red Crow, also took your playtime into consideration. <br/>
 
-                        As a rule of thumb: <em>"skill on leaderboard" = skill-rating - uncertainty</em>.
+                        TLDR: <em>"skill on leaderboard" = skill-rating - uncertainty</em>.
                    </p>
                     <p>
                         How does this relate to the leaderboard?
                         When we decided on creating one, we had to find a stat to use as ranking. In our opinion win-rate and kill-death-ratio are not suitable for that, because they are easily skewed. For example:
                         <blockquote>
                             In Season 2 (Dust Line) our team managed to go on a 80 win streak in the ranked playlist. <br/>
-                            We even managed to bring on of our mates up to a 54 win-rate (thats 98%). 
+                            We even managed to bring on of our mates up to a 54 win/loss ratio (thats 98%).
                             There was (and is) obviously way better players than us, but we had the nice stats.
                         </blockquote>
                         We'd be "better" than most players, even though we really are not (well, except one). <br/>
@@ -59,10 +59,10 @@ export default {
                 </Question>
                 <Question question="I searched for my name, but it says that I haven't played the game yet.">
                     <p>
-                        this Error can be triggered in multiple ways:  
+                        The error can be triggered in multiple ways:
                         <ul>
                             <li>we didn't grab your data yet. Try again in a few minutes</li>
-                            <li>you're looking on the wrong patform (see links in the top of the menu)</li>
+                            <li>you're looking on the wrong platform (see links at the top of the menu)</li>
                             <li>you actually don't own the game, or didn't play</li>
                         </ul>
                     </p>
@@ -75,8 +75,8 @@ export default {
                 </Question>
                 <Question question="I want to develop my own stuff with r6db data. Do you guys have an API?">
                     <p>
-                        the short answer is yes. Shoot us a mail and we'll get you going. Detailed documentation is in the works. <br />
-                        <em>Just to make it clear: We do and can NOT give any guarantee about uptime and availability.</em> While NaugtyMuppet does give his best, it is still a hobby project.
+                        The short answer is yes. Shoot us a mail and we'll get you going. Detailed documentation is in the works. <br />
+                        <em>Just to make it clear: We do and can NOT give any guarantee about uptime and availability.</em> While Muppet does give his best, it is still a hobby project.
                     </p>
                 </Question>
                 <Question question="What technologies do you guys use?">
@@ -86,7 +86,7 @@ export default {
                     <p>
                         On the frontend, we use <a href="https://mithril.js.org/">Mithril</a> (highly recommended) as framework with <a href="https://github.com/visionmedia/page.js/">PageJS</a> for routing and <a href="https://github.com/Yomguithereal/baobab">Baobab</a> for state management.  
                         <br />
-                        We had a strong focus on high performance and small size with visuals coming second. This is the reason why we use so little images and animations.  
+                        We had a strong focus on high performance and small size with visuals coming second. This is the reason why we use so little images and animations.
                         Most images you see are actually SVGs, which scale perfectly and have a small size. <br />
                         This allows us to load the site in 200ms (for the landing page) on DSL, and 1.1 seconds on a good 3G connection.
                     </p>
