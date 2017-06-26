865d887eba72bca6173c9596aa9fbc47b60a270b
Naughtimus Muppetus <info@r6db.com>
2017-06-26 18:58:56 +0300

    fix: do not show #1 on people who do not have a placement

--- src/app/components/Pages/Profile/stats/modules/RankOverview/index.js
+++ src/app/components/Pages/Profile/stats/modules/RankOverview/index.js
@@ -13,7 +13,7 @@ const CurrentRank = {
                 <div className="rankoverview-current-icon">
                     <Icon glyph={GLYPHS["RANK" + attrs.rank]} />
                 </div>
-                <div className="rankoverview-current-placement">#{attrs.placement + 1}</div>
+                <div className="rankoverview-current-placement">{ attrs.placement ? "#" + (attrs.placement + 1) : ""}</div>
                 <div className="rankoverview-current-region">{attrs.region}</div>
             </div>
         )
