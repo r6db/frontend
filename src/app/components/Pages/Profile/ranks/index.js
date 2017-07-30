6f7be360abdccb12021c08c524a95cccc155ca6f
Lars Brix <laxisbrix@gmail.com>
2017-07-30 16:00:43 +0200

    fix table layout + shwo unranked

--- src/app/components/Pages/Profile/ranks/index.js
+++ src/app/components/Pages/Profile/ranks/index.js
@@ -51,7 +51,7 @@ export default {
                     return false;
                 }
             }
-            if (showUnranked) {
+            if (!showUnranked) {
                 return x.rank !== 0;
             }
             return true;
