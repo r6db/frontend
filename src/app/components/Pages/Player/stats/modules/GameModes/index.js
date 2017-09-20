20328273c101a57a06a302393312792c0a134c99
Laxis <laxis@r6db.com>
2017-09-20 19:33:10 +0200

    add some lodash ( resolves r6db/r6db#15 )

--- src/app/components/Pages/Player/stats/modules/GameModes/index.js
+++ src/app/components/Pages/Player/stats/modules/GameModes/index.js
@@ -1,4 +1,5 @@
 import * as m from "mithril";
+import * as get from "lodash/get";
 import "./gamemodes.scss";
 import Stat from "components/misc/Stat"
 import { getWinChance } from "lib/stats";
@@ -7,9 +8,9 @@ const ModeStats = {
     view({ attrs }) {
         return (
             <div className="player-module-content mode">
-                <Stat label="max score">{attrs.bestScore}</Stat>
-                <Stat label="wins">{attrs.won}</Stat>
-                <Stat label="losses">{attrs.lost}</Stat>
+                <Stat label="max score">{get(attrs, "bestScore", 0)}</Stat>
+                <Stat label="wins">{get(attrs, "won", 0)}</Stat>
+                <Stat label="losses">{get(attrs, "lost", 0)}</Stat>
                 <Stat label="win rate">{getWinChance(attrs)}</Stat>
             </div>
         )
@@ -23,31 +24,31 @@ export default {
             <div className="player-module gamemodes">
                 <div className="player-module-header">Bomb</div>
                 <div className="player-module-content mode">
-                    <Stat label="max score">{attrs.stats.bomb.bestScore}</Stat>
-                    <Stat label="wins">{attrs.stats.bomb.won}</Stat>
-                    <Stat label="losses">{attrs.stats.bomb.lost}</Stat>
+                    <Stat label="max score">{get(attrs, "stats.bomb.bestScore", 0)}</Stat>
+                    <Stat label="wins">{get(attrs, "stats.bomb.won", 0)}</Stat>
+                    <Stat label="losses">{get(attrs, "stats.bomb.lost", 0)}</Stat>
                     <Stat label="win rate">{getWinChance(attrs.stats.bomb)}</Stat>
                 </div>
 
                 <div className="player-module-header">Secure Area</div>
                 <div className="player-module-content mode">
-                    <Stat label="max score">{attrs.stats.secure.bestScore}</Stat>
-                    <Stat label="wins">{attrs.stats.secure.won}</Stat>
-                    <Stat label="losses">{attrs.stats.secure.lost}</Stat>
+                    <Stat label="max score">{get(attrs, "stats.secure.bestScore", 0)}</Stat>
+                    <Stat label="wins">{get(attrs, "stats.secure.won", 0)}</Stat>
+                    <Stat label="losses">{get(attrs, "stats.secure.lost", 0)}</Stat>
                     <Stat label="win rate">{getWinChance(attrs.stats.secure)}</Stat>
-                    <Stat label="first to secure">{attrs.stats.general.serverAggression}</Stat>
-                    <Stat label="secured">{attrs.stats.general.serversHacked}</Stat>
-                    <Stat label="secure denied">{attrs.stats.general.serverDefender}</Stat>
+                    <Stat label="first to secure">{get(attrs, "stats.general.serverAggression", 0)}</Stat>
+                    <Stat label="secured">{get(attrs, "stats.general.serversHacked", 0)}</Stat>
+                    <Stat label="secure denied">{get(attrs, "stats.general.serverDefender", 0)}</Stat>
                 </div>
 
                 <div className="player-module-header">Hostage</div>
                 <div className="player-module-content mode">
-                    <Stat label="max score">{attrs.stats.hostage.bestScore}</Stat>
-                    <Stat label="wins">{attrs.stats.hostage.won}</Stat>
-                    <Stat label="losses">{attrs.stats.hostage.lost}</Stat>
+                    <Stat label="max score">{get(attrs, "stats.hostage.bestScore", 0)}</Stat>
+                    <Stat label="wins">{get(attrs, "stats.hostage.won", 0)}</Stat>
+                    <Stat label="losses">{get(attrs, "stats.hostage.lost", 0)}</Stat>
                     <Stat label="win rate">{getWinChance(attrs.stats.hostage)}</Stat>
-                    <Stat label="extractions">{attrs.stats.general.hostageRescue}</Stat>
-                    <Stat label="extractions denied">{attrs.stats.general.hostageDefense}</Stat>
+                    <Stat label="extractions">{get(attrs, "stats.general.hostageRescue", 0)}</Stat>
+                    <Stat label="extractions denied">{get(attrs, "stats.general.hostageDefense", 0)}</Stat>
                 </div>
             </div>
         )
