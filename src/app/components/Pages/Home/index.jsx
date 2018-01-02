c24a3526e17ce94d444c661a4125ac0c81368971
Laxis <laxis@r6db.com>
2018-01-02 12:52:43 +0100

    aaand if forgot to press save

--- src/app/components/Pages/Home/index.jsx
+++ src/app/components/Pages/Home/index.jsx
@@ -7,13 +7,8 @@ const idRegex = /[\da-zA-Z]{8}-[\da-zA-Z]{4}-[\da-zA-Z]{4}-[\da-zA-Z]{4}-[\da-zA
 const showPlayer = id => e => page("/player/" + id);
 
 export default {
-<<<<<<< Updated upstream
-    oncreate({ dom }) {
-        const search = dom.querySelector(".searchbar__input");
-=======
     oncreate({}) {
         const search = document.querySelector(".page__headcontent .searchbar__name");
->>>>>>> Stashed changes
         if (search && !window.ontouchstart) {
             search.focus();
         }
