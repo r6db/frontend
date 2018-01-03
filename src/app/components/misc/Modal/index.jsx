28a2bfd253de8f168e25dc90ed9f988755fc7efc
Lars <mail@laxis.me>
2018-01-03 18:40:39 +0100

    add modal component

---
+++ src/app/components/misc/Modal/index.jsx
@@ -0,0 +1,28 @@
+import * as m from "mithril";
+import Portal from "mithril-portal";
+import "./modal.scss";
+
+const Modal = {
+    view({ attrs, children }) {
+        return (
+            <Portal>
+                <div className="modal">
+                    <div onclick={attrs.onclose} className="modal__background" />
+                    <div className="modal__container">
+                        <div className="modal__item">
+                            <div className="modal__head">
+                                <header className="modal__title">{attrs.title}</header>
+                                <span onclick={attrs.onclose} className="modal__close">
+                                    ✖
+                                </span>
+                            </div>
+                            <div className="modal__body">{children}</div>
+                        </div>
+                    </div>
+                </div>
+            </Portal>
+        );
+    },
+};
+
+export default Modal;
