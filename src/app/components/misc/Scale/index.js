7dcc1d95e54f23adf7dd58280266ddc46fc21254
Laxis <laxis@r6db.com>
2018-01-14 14:11:48 +0100

    fix scale "mid" type

--- src/app/components/misc/Scale/index.js
+++ src/app/components/misc/Scale/index.js
@@ -4,54 +4,54 @@ import "./scale.scss";
 const asString = val => (typeof val === "number" ? val.toFixed(2) : val.toString());
 
 const defaultScale = {
-    low: 0.5,
     "med-low": 0.8,
+    med: 1,
     "med-high": 1.2,
     high: 1.5,
     nutty: 2.5,
 };
 
 const kdScale = {
-    low: 0.5,
     "med-low": 0.8,
+    med: 1,
     "med-high": 1.3,
     high: 1.9,
     nutty: 2.5,
 };
 const kdaScale = {
-    low: 0.8,
     "med-low": 1.2,
+    med: 1.6,
     "med-high": 1.9,
     high: 2.4,
     nutty: 3,
 };
 
 const winlossScale = {
-    low: 35,
     "med-low": 45,
+    med: 50,
     "med-high": 55,
     high: 65,
     nutty: 75,
 };
 
 const headshotScale = {
-    low: 15,
     "med-low": 25,
-    "med-high": 35,
-    high: 45,
-    nutty: 55,
+    med: 35,
+    "med-high": 45,
+    high: 55,
+    nutty: 60,
 };
 
 const kprScale = {
-    low: 0.2,
     "med-low": 0.7,
+    med: 0.85,
     "med-high": 1,
     high: 1.3,
     nutty: 1.6,
 };
 const survivalScale = {
-    low: 15,
     "med-low": 25,
+    med: 0.3,
     "med-high": 35,
     high: 45,
     nutty: 55,
