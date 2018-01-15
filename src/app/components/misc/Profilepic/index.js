6d16e2c6777ff1deb9e5ee25cadd7b6820f67bec
Marco Vockner <marcopixel@live.de>
2018-01-15 19:22:44 +0100

    Fixed lazyload only affecting local images, improved image rendering on Chrome when loading in.

---
+++ src/app/components/misc/Profilepic/index.js
@@ -0,0 +1,36 @@
+import * as m from "mithril";
+import lozad from "lozad";
+import { connect } from "lib/store/connect";
+import "./profilepic.scss";
+
+const ProfilePic = {
+    oninit({ attrs, state, dom }) {
+        state.src = attrs.isConsole
+            ? `//ubisoft-avatars.akamaized.net/${attrs.id}/default_146_146.png`
+            : `//uplay-avatars.s3.amazonaws.com/${attrs.id}/default_146_146.png`;
+
+        state.onerror = e => {
+            e.preventDefault();
+            e.stopPropagation();
+            state.src = "/assets/noavatar.svg";
+            e.target.src = "/assets/noavatar.svg";
+        };
+    },
+    oncreate() {
+        const observer = lozad(".profile-pic", {
+            load(el) {
+                el.src = el.dataset.src;
+                el.onload = function() {
+                    el.setAttribute("data-created", "true");
+                };
+            }
+        });
+        observer.observe();
+    },
+    view: vnode => m("img.profile-pic", { "data-src": vnode.state.src, onerror: vnode.state.onerror }),
+};
+
+const mapStateToProps = getState => ({
+    isConsole: getState().platform !== "PC",
+});
+export default connect(mapStateToProps)(ProfilePic);
