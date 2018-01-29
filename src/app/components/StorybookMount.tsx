3bc7d651091d6a7b899eda4e3fe5b2da1fe08ca2
Lars Brix <lars.brix@vispiron.de>
2018-01-29 14:00:20 +0100

    add storybook

---
+++ src/app/components/StorybookMount.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+
+import "./base.scss";
+import "./app.scss";
+
+export default function App(props) {
+    return props.children
+}
--- src/app/components/misc/Portal.tsx
+++ src/app/components/misc/Portal.tsx
@@ -1,7 +1,17 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
-const getRoot = () => document.getElementById("portal");
+const getRoot = () => {
+    const mount = document.getElementById("portal")
+    if (mount) {
+        return mount;
+    } else {
+        const m = document.createElement("div");
+        m.id = "portal";
+        document.body.appendChild(m);
+        return m;
+    }
+};
 
 export default function Portal(props) {
     return ReactDOM.createPortal(props.children, getRoot());
