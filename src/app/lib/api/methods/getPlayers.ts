6dddf713df22b8ede49bf0cc185b5eaa8cc91f2d
Laxis <8034086+LaxisB@users.noreply.github.com>
2018-07-02 18:33:58 +0200

    handle cases of 100+ favs (#66)

--- src/app/lib/api/methods/getPlayers.ts
+++ src/app/lib/api/methods/getPlayers.ts
@@ -1,15 +1,20 @@
 import { v2Api } from "lib/constants";
 import { failEarly, getHeaders } from "../utils";
 import handleResponse from "../handlePlayer";
+import * as chunk from "lodash/chunk";
+import * as flatten from "lodash/flatten";
 
 const getPlayers = ids => {
-    return fetch(`${v2Api}/players?ids=${ids.join(",")}`, {
-        headers: getHeaders(),
+    return fetch(`${v2Api}/players?ids=${ids.join(",")}&platform=PC`, {
+        headers: getHeaders()
     })
         .then(failEarly)
         .then(res => res.json());
 };
 
-export default function(id) {
-    return getPlayers(id).then(x => x.map(handleResponse));
+export default function(ids) {
+    const proms = chunk(ids, 100).map(chunk => getPlayers(ids));
+    return Promise.all(proms)
+        .then(flatten)
+        .then(x => x.map(handleResponse));
 }
