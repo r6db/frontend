42af0ef6280ebe9b2b22168898de230675754f12
Naughtimus Muppetus <info@r6db.com>
2018-07-06 22:22:56 +0300

    fix: malformed urls in sentry upload

--- uploadsourcemaps.sh
+++ uploadsourcemaps.sh
@@ -7,8 +7,7 @@ if [[ -z "${VERSION}" ]]; then
 else
 
     # Create release
-    curl $SENTYURL/api/hooks/release/builtin/2/85e2d7d2ab9add7563cdafaecbd46596ab4c8b8058829832df751e3aca30d089/ \
-        -H "Authorization: Bearer $TOKEN" \
+    curl $SENTRYURL/api/hooks/release/builtin/2/85e2d7d2ab9add7563cdafaecbd46596ab4c8b8058829832df751e3aca30d089/ \
         -X POST \
         -H 'Content-Type: application/json' \
         -d "{\"version\": \"$VERSION\"}" -s
@@ -17,7 +16,7 @@ else
 
     for filename in build/*.js.map; do
         echo "Uploading: $filename"
-        curl $SENTYURL/api/0/projects/r6db/frontend/releases/$VERSION/files/ \
+        curl $SENTRYURL/api/0/projects/r6db/frontend/releases/$VERSION/files/ \
             -X POST \
             -H "Authorization: Bearer $TOKEN" \
             -F file=@$filename \
